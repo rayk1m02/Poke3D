28ef0ee37fb36fa179f8a349d3ad756eeed05a07
rayk1m02 <hkapray@gmaill.com>
2024-11-16 20:42:59 -0600

    initial commit

---
+++ Poke3DTests/Poke3DTests.swift
@@ -0,0 +1,17 @@
+//
+//  Poke3DTests.swift
+//  Poke3DTests
+//
+//  Created by Raymond Kim on 11/16/24.
+//
+
+import Testing
+@testable import Poke3D
+
+struct Poke3DTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
